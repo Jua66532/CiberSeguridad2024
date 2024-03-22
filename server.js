8a22ae285836e3ec3c294fbf34e63480a52ad1da
Jua66532 <servinjuan665@gmail.com>
2024-03-22 11:04:15 -0600

    Agregar popo

--- server.js
+++ server.js
@@ -1,11 +1,59 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 const app = express()
 
-app.get('/', function (req, res) 
-{
-    //res.send(Hello world)
-  res.sendFile(__dirname + "/index.html");
+// Middleware para analizar los datos del formulario
+app.use(express.urlencoded({ extended: true }));
+// Middleware de logging
+app.use(morgan('dev'));
+
+// Conexión a MongoDB Atlas
+mongoose.connect('mongodb+srv://servinjuan665:57stwP7wHRf10OOK@clusterciberseg.43zf6hd.mongodb.net/?retryWrites=true&w=majority&appName=ClusterCiberSeg', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Conexión a MongoDB Atlas establecida');
+})
+.catch((err) => {
+  console.error('Error al conectar a MongoDB Atlas:', err);
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/index.html')
 })
 
 
-app.listen(3000)
+// Definir el esquema de datos
+const Schema = mongoose.Schema;
+const contactSchema = new Schema({
+  email: String,
+  contraseña: String
+});
+
+// Crear el modelo Contact basado en el esquema
+const Contact = mongoose.model('Contact', contactSchema);
+
+app.listen(3000,()=>{
+console.log("escuchando ando")
+})
+
+// Manejar la solicitud POST para /contact
+app.post('/contact', async function (req, res) {
+  const { contraseña, email } = req.body;
+  // Crear una nueva instancia de Contact con los datos recibidos
+  const newContact = new Contact({
+    contraseña,
+    email,
+  });
+  try {
+    // Guardar el nuevo contacto en la base de datos
+    await newContact.save();
+    console.log('Nuevo contacto guardado en la base de datos:', newContact);
+    res.send('Mensaje enviado correctamente');
+  } catch (err) {
+    console.error('Error al guardar el contacto en la base de datos:', err);
+    res.status(500).send('Error al guardar el contacto en la base de datos');
+  }
+});
